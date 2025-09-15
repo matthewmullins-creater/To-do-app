0a58e5dcc2445e03c62ac89c6f536d9fce92c8e0
matthewmullins-creater <matthewmullins1219@gmail.com>
2025-09-15 15:01:16 -0500

    Add customized forms.py

--- todolist/forms.py
+++ todolist/forms.py
@@ -3,6 +3,14 @@ from .models import Todolist
 
 
 class TodoListForm(forms.Form):
-    text = forms.CharField(max_length=45,
-                           widget=forms.TextInput(
-                               attrs={'class': 'form-control', 'placeholder': 'Enter todo e.g. Grocery Shopping', 'aria-label': 'Todo', 'aria-describedby': 'add-btn'}))
+     text = forms.CharField(
+        max_length=45,
+        widget=forms.TextInput(
+            attrs = {
+                'class': 'form-control',
+                'placeholder': 'Enter todo e.g. Grocery Shopping',
+                'aria-label': 'Todo', 
+                'aria-describedby': 'add-btn'
+            }
+        )
+    )
